e9d921c9047ae2f6ddba2b2d15c0b260e134ff0e
Talon <asd@asd.com>
2018-01-09 15:28:32 +0100

    added 'rimraf' and 'mkdirp', removed development, copied files into resources/static

--- src/main/java/com/fabio/springbootangular4one/SpringBootAngular4OneApplication.java
+++ src/main/java/com/fabio/springbootangular4one/SpringBootAngular4OneApplication.java
@@ -20,14 +20,14 @@ public class SpringBootAngular4OneApplication {
 	@Bean
 	CommandLineRunner runner(TaskService taskService){
 		return args -> {
-			taskService.save( new Task("Create Spring Boot Application", LocalDate.now(), true) );
-			taskService.save( new Task("Create Spring Project Packages", LocalDate.now().plus(1, ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Create the Task Domain Class", LocalDate.now().plus(3,ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Create service and repository classes", LocalDate.now().plus(5,ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Create the command line runner to load data", LocalDate.now().plus(8, ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Create the required configuration properties", LocalDate.now().plus(10,ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Run the Spring Boot Application", LocalDate.now().plus(12,ChronoUnit.DAYS), false) );
-			taskService.save( new Task("Check the H2 Console for the initial data", LocalDate.now().plus(13,ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 1", LocalDate.now(), true) );
+			taskService.save( new Task("Task number 2", LocalDate.now().plus(1, ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 3", LocalDate.now().plus(3,ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 4", LocalDate.now().plus(5,ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 5", LocalDate.now().plus(8, ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 6", LocalDate.now().plus(10,ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 7", LocalDate.now().plus(12,ChronoUnit.DAYS), false) );
+			taskService.save( new Task("Task number 8", LocalDate.now().plus(13,ChronoUnit.DAYS), false) );
 		};
 	}
 
